be0a399113b07e7b09f4f2b870a531ad3620fd92
hoang nv <hoangproo2624@gmail.com>
2026-09-19 10:13:54 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-
-  // Tên repository GitHub của bạn
-  basePath: "/food_ticket",
-  assetPrefix: "/food_ticket/",
-
-  images: {
-    unoptimized: true,
-  },
-
-  trailingSlash: true,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
